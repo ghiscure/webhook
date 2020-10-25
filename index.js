c8681df1a33e909a6f868f697424c2f90bbf5892
afrizaloky <afrizaloky@outlook.com>
2020-10-25 13:12:34 +0700

    change version api

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ function callSendAPI(sender_psid, response) {
 
   // Send the HTTP request to the Messenger Platform
   request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "uri": "https://graph.facebook.com/v8.0/me/messages",
     "qs": { "access_token": PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
